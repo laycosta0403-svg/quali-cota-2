bfe119440a0b8d3d6fa79b813bbe01714f6b6e3e
laycosta0403-svg <laycosta0403@gmail.com>
2026-07-20 11:29:54 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Quali Cota 2.0",
+    page_icon="📦",
+    layout="wide"
+)
+
+st.title("📦 Quali Cota 2.0")
+
+st.success("Projeto iniciado com sucesso 🚀")
